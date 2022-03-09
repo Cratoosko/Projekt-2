c8eabac2d8324f26509d9f01150dfae1239db361
OzanGvn <crathus69@gmail.com>
2022-03-09 14:57:58 +0100

    Added clock

--- javascript.js
+++ javascript.js
@@ -22,6 +22,35 @@ function updateTimer() {
     timerOutput.innerHTML = currentTime;
 }
 
+document.getElementById("timer-display").innerHTML = currentTime+1 + " seconds";
+
 function timerIsUp() {
-  alert("Your time is up, Mr.");
+  alert("Your time is up!");
 }
+
+function timeDisplay() {
+  var date = new Date();
+  var hour = date.getHours();
+  var min = date.getMinutes();
+  var sec = date.getSeconds();
+  hour = formatTimeDisplay(hour)
+  min = formatTimeDisplay(min)
+  sec = formatTimeDisplay(sec)
+  document.getElementById("clock").innerHTML = hour + ":" + min + ":" + sec;
+}
+
+
+var t = 
+setTimeout(function(){ 
+timeDisplay() }, 1000);
+
+function formatTimeDisplay(s) {
+  if(s < 10) {
+    return "0" + s;
+  }
+  else {
+    return s;
+  }
+}
+
+timeDisplay();
---
+++ javascriptclock.js
@@ -0,0 +1,26 @@
+function timeDisplay() {
+    var date = new Date();
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+    hour = formatTimeDisplay(hour)
+    min = formatTimeDisplay(min)
+    sec = formatTimeDisplay(sec)
+    document.getElementById("clock").innerHTML = hour + ":" + min + ":" + sec;
+  }
+
+  var t = 
+  setTimeout(function(){ 
+  timeDisplay() }, 1000);
+  
+  function formatTimeDisplay(s) {
+    if(s < 10) {
+      return "0" + s;
+    }
+    else {
+      return s;
+    }
+  }
+  
+  timeDisplay();
+  
